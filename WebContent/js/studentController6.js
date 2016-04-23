892d9b018e019a4b5b6a71d426220ce0be95d703
Daniel-Dias <daniel.dias.analistati@gmail.com>
2016-04-22 22:51:22 -0300

    Adicionado Exemplo10 Includes - rodar em algum servidor web

---
+++ WebContent/js/studentController6.js
@@ -0,0 +1,23 @@
+angular.module("studentApp6", []).controller(
+		"studentController6",
+		function($scope) {
+
+			$scope.student = {
+				firstName : "Mahesh",
+				lastName : "Parashar",
+				fees:500,
+				subjects:[
+				          {name:'Physics',marks:70},
+				          {name:'Chemistry',marks:80},
+				          {name:'Math',marks:65},
+				          {name:'English',marks:75},
+				          {name:'Hindi',marks:67}
+				          ],
+				fullName : function() {
+					var studentObject;
+					studentObject = $scope.student;
+					return studentObject.firstName + " "
+							+ studentObject.lastName;
+				}
+			};
+		});
